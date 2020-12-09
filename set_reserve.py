2ef18bccb42217ae8dacbbee295ee6eb11d88649
natrlHy <natrlhy@gmail.com>
2020-12-09 11:33:54 -0800

    set backup reserve

---
+++ set_reserve.py
@@ -0,0 +1,77 @@
+import logging
+import sys
+import time
+
+import requests
+
+import constants as C
+from common import apicall
+from teslatoken import Token
+
+logging.basicConfig(
+    filename="powerwall.log",
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+)
+
+
+def productlists():
+
+    token = Token()
+    response = apicall(
+        C.PRODUCTS_ENDPOINT,
+        "GET",
+        headers={"Authorization": "Bearer " + token.tokenstr},
+    )
+
+    if response.status_code != 200:
+        logging.error("Couldn't get products. Reason: %s" % (str(response)))
+        raise Exception("Couldn't get products. Reason: %s" % (str(response)))
+    return response.json().get("response", [])
+
+
+def getsolarproduct(productlist):
+
+    if not isinstance(productlist, list):
+        productlist = []
+
+    energysites = {
+        product["energy_site_id"]: product
+        for product in productlist
+        if "energy_site_id" in product
+    }
+    return energysites
+
+
+def setreserve(siteid, respct):
+
+    respct = 100
+    if not isinstance(siteid, int):
+        siteid = ""
+
+    if siteid:
+        token = Token()
+        params = {"backup_reserve_percent": respct}
+        response = apicall(
+            C.OPERATION_ENDPOINT.format(siteid),
+            "POST",
+            headers={"Authorization": "Bearer " + token.tokenstr},
+            params=params,
+        )
+        if response.status_code != 200:
+            logging.error(
+                "Couldn't change reserve percent. Reason: %s" % (str(response))
+            )
+            raise Exception(
+                "Couldn't change reserve percent. Reason: %s" % (str(response))
+            )
+        logging.info(response.json())
+
+
+if len(sys.argv) == 2:
+    respct = sys.argv[1]
+    energy = getsolarproduct(productlists())
+    if energy:
+        siteid = list(energy.keys())[0]
+        setreserve(siteid, respct)
+    logging.info("Changed mode to: " + respct)
